71d6db1bba4c5900a66581b60a9e82d0f8d60d29
BeiyanYunyi <14120445+BeiyanYunyi@users.noreply.github.com>
2024-09-30 17:22:44 +0800

    feat(aio): :sparkles: add mount function

--- src/aio.ts
+++ src/aio.ts
@@ -1,3 +1,17 @@
+import { createApp } from 'vue'
 import App from './App.vue'
 
-export default App
+function mount(props: {
+  attachImageButtonText?: string
+  sendButtonText?: string
+  sendingButtonText?: string
+  sentSuccessButtonText?: string
+  sentFailedButtonText?: string
+  targetUrl?: string
+  textContentPlaceholder?: string
+  contactContentPlaceholder?: string
+}, rootContainer: string | Element) {
+  createApp(App, props).mount(rootContainer)
+}
+
+export default mount
